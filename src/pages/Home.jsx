09d439f70cffa5191f3d916e4dae1624a5c62ddc
MriduJain23 <mridujain2005@gmail.com>
2026-02-02 19:11:11 +0530

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
-import Navbar from "../components/Navbar";
 import SearchForm from "../components/SearchForm";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
       <div className="orb blue"></div>
 <div className="orb purple"></div>
 
